827c6179ad0d343423c71fbb4e9f20ec680356c7
YurikoU <76763897+YurikoU@users.noreply.github.com>
2021-03-31 11:08:34 -0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace In_class_Assignment2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonRollADice = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Black", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(138, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 96);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "1";
+            // 
+            // buttonRollADice
+            // 
+            this.buttonRollADice.Location = new System.Drawing.Point(92, 167);
+            this.buttonRollADice.Name = "buttonRollADice";
+            this.buttonRollADice.Size = new System.Drawing.Size(166, 63);
+            this.buttonRollADice.TabIndex = 1;
+            this.buttonRollADice.Text = "Roll-A-Dice";
+            this.buttonRollADice.UseVisualStyleBackColor = true;
+            this.buttonRollADice.Click += new System.EventHandler(this.buttonRollADice_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(381, 280);
+            this.Controls.Add(this.buttonRollADice);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "DiceRoll";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonRollADice;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace In_class_Assignment2
+{
+    public partial class Form1 : Form
+    {
+        public bool diceIsRolling = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonRollADice_Click(object sender, EventArgs e)
+        {
+            if (!diceIsRolling)
+            {
+                diceIsRolling = true;
+                buttonRollADice.Text = "Rolling it !!";
+                Random random = new Random();
+                label1.Text = Convert.ToString(random.Next(1,7));
+            }
+            else {
+                diceIsRolling = false;
+                buttonRollADice.Text = "Roll-A-Dice";
+            }
+        }
+    }
+}
