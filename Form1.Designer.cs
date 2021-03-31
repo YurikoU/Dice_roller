b43b9f68d8684f42200ab798ad86313e1421a791
YurikoU <76763897+YurikoU@users.noreply.github.com>
2021-03-31 13:32:14 -0700

    Add files via upload

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,24 +30,17 @@ namespace In_class_Assignment2
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.label1 = new System.Windows.Forms.Label();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.buttonRollADice = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI Black", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(138, 39);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(75, 96);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "1";
             // 
             // buttonRollADice
             // 
-            this.buttonRollADice.Location = new System.Drawing.Point(92, 167);
+            this.buttonRollADice.Location = new System.Drawing.Point(101, 205);
             this.buttonRollADice.Name = "buttonRollADice";
             this.buttonRollADice.Size = new System.Drawing.Size(166, 63);
             this.buttonRollADice.TabIndex = 1;
@@ -57,28 +50,49 @@ namespace In_class_Assignment2
             // 
             // timer1
             // 
-            this.timer1.Interval = 200;
+            this.timer1.Interval = 300;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(179, 46);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(112, 112);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Black", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(78, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 96);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(381, 280);
+            this.ClientSize = new System.Drawing.Size(371, 310);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.buttonRollADice);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "DiceRoll";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button buttonRollADice;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,8 @@ In-class Assignment 2
 */
 
 
+using In_class_Assignment2.Properties;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 
@@ -25,37 +19,60 @@ namespace In_class_Assignment2
         public bool diceIsRolling = false;
         Random random = new Random();
 
-
         public Form1()
         {
             InitializeComponent();
         }
 
-
         private void buttonRollADice_Click(object sender, EventArgs e)
         {
             if (!diceIsRolling)
             {  
-                //If the dice pauses
+                //If the dice paused when a user clicked the button
                 diceIsRolling = true;
                 timer1.Enabled = true;
+                timer1.Start();
                 buttonRollADice.Text = "Rolling it !!";
             }
             else 
             {
-                //If the dice is rolling
+                //If the dice was rolling
                 diceIsRolling = false;
                 timer1.Enabled = false;
+                timer1.Stop();
                 buttonRollADice.Text = "Roll-A-Dice";
             }
         }
 
 
-        //Wile the timer property is enabled
+        //Wile the timer is enabled
         private void timer1_Tick(object sender, EventArgs e)
         {
-            //Display a random number by 0.2 second
+            //Display a random number by 0.3 second
             label1.Text = Convert.ToString(random.Next(1, 7));
+
+            //Display a proper dice face based on the random number
+            switch (label1.Text)
+            {
+                case "1":
+                    pictureBox1.Image = Resources.dice1;
+                    break;
+                case "2":
+                    pictureBox1.Image = Resources.dice2;
+                    break;
+                case "3":
+                    pictureBox1.Image = Resources.dice3;
+                    break;
+                case "4":
+                    pictureBox1.Image = Resources.dice4;
+                    break;
+                case "5":
+                    pictureBox1.Image = Resources.dice5;
+                    break;
+                case "6":
+                    pictureBox1.Image = Resources.dice6;
+                    break;
+            }
         }
     }
 }
